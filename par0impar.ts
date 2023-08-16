f39a642b2c7999025ac044004482ff6162036640
George Detmering <georgedetmering@gmail.com>
2023-08-16 16:05:39 -0300

    Create par0impar.ts

---
+++ par0impar.ts
@@ -0,0 +1,11 @@
+// Determine se um número é par, impar ou zero utilizando uma estrutura if e o operador %.
+
+const numero = 0;
+
+if (numero === 0) {
+    console.log("O número é zero.");    
+} else if (numero % 2 === 0) {
+    console.log("O número é par.");
+} else {
+    console.log("O número é impar.");
+}
