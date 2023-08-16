90b5b28e99b31565d56006d69103e5e4ea24cc5a
George Detmering <georgedetmering@gmail.com>
2023-08-16 15:53:59 -0300

    Create positivonegativozero.ts

---
+++ positivonegativozero.ts
@@ -0,0 +1,11 @@
+// Verifique se um número é positivo, negativo ou zero usando declarações if, else if e else.
+
+const numero = 12;
+
+if (numero > 0) {
+    console.log("O número é positivo.");
+} else if (numero < 0) {
+    console.log("O número é negativo.");
+} else {
+    console.log("O número é zero.");
+}
